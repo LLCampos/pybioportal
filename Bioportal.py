abc94fa490c320c43dcc7cc6da07e3d119a18d52
LLCampos <luis1212@sapo.pt>
2016-11-09 18:56:46 +0000

    first commit

---
+++ Bioportal.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import json
+import requests
+from requests import HTTPError
+
+
+class Bioportal(object):
+
+    '''A Python binding of the BioPortal REST API
+    (http://data.bioontology.org/documentation)'''
+
+    BASE_URL = 'http://data.bioontology.org'
+
+    def __init__(self):
+
+        # There should be a file named "bioportal.json" in the root of the
+        # project. It should have the following structure:
+        # {"api_key": "YOUR API KEY"}
+        # You can get the API Key by signing up at BioPortal website.
+        with open("bioportal.json") as f:
+            self.apikey = json.load(f)["api_key"]
+
+    def classes(self, search_query, **kwargs):
+
+        # http://data.bioontology.org/documentation#nav_search
+
+        endpoint = '/search'
+        full_url = Bioportal.BASE_URL + endpoint
+
+        payload = kwargs
+        payload['q'] = search_query
+        payload['apikey'] = self.apikey
+
+        return self._bioportal_api_request(full_url, payload)
+
+    def annotator(self, text, only_terms_names=False, **kwargs):
+
+        '''If the 'only_terms_names' argument is true, return only a set of
+        the terms annotated. If the term found is a synonym, it will return  '''
+
+        # http://data.bioontology.org/documentation#nav_annotator
+
+        endpoint = '/annotator'
+        full_url = Bioportal.BASE_URL + endpoint
+
+        payload = kwargs
+        payload['text'] = text
+        payload['apikey'] = self.apikey
+
+        complete_annotations = self._bioportal_api_request(full_url, payload)
+
+        if only_terms_names:
+            return self._extract_terms_names_from_annotations(complete_annotations)
+        else:
+            return complete_annotations
+
+    def recommender(self, text_or_keywords, **kwargs):
+
+        # http://data.bioontology.org/documentation#nav_recommender
+
+        endpoint = '/recommender'
+        full_url = Bioportal.BASE_URL + endpoint
+
+        payload = kwargs
+        payload['input'] = text_or_keywords
+        payload['apikey'] = self.apikey
+
+        return self._bioportal_api_request(full_url, payload)
+
+    def _bioportal_api_request(self, url, payload):
+
+        r = requests.get(url, params=payload)
+        json_response = r.json()
+
+        try:
+            # This will raise an HTTPError if the HTTP request returned an
+            # unsuccessful status code.
+            r.raise_for_status()
+        except HTTPError:
+            error_messages = json_response['errors']
+            joint_error_messages = '\n'.join(error_messages)
+            raise HTTPError(joint_error_messages)
+
+        return json_response
+
+    def _extract_terms_names_from_annotations(self, complete_annotations):
+        all_terms = []
+
+        for annotated_class in complete_annotations:
+            for annotation in annotated_class['annotations']:
+                all_terms.append(annotation['text'])
+
+        return set(all_terms)
