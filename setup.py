eb11ed9dddef8f8cdc0206e427f7ff53a817d7b1
LLCampos <luis1212@sapo.pt>
2016-11-21 08:39:56 +0000

    correct setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ except ImportError:
 setup(
     name='pybioportal',
     version='0.1',
+    packages=['pybioportal'],
     description="A Python binding of the BioPortal REST API.",
     author='Lu\xc3\xads Campos',
     author_email='luis.filipe.lcampos@gmail.com',
